0e8cde1ccad7aa5c968fcb8b399079a6b12aa844
Denis Toropin <denis.toropin@yandex.ru>
2019-02-07 15:43:58 +0300

    refactoring structure project

--- gulpfile.js
+++ gulpfile.js
@@ -33,14 +33,14 @@ gulp.task('js-vendor', function () {
 	return gulp.src(js_vendor)
 		.pipe(concat('vendor.js'))
 		.pipe(minify({noSource: true}))
-		.pipe(gulp.dest('app'));
+		.pipe(gulp.dest('app/js'));
 });
 
 // Js
 gulp.task('js', function () {
 	return gulp.src('app/js/*.js')
 		.pipe(minify({noSource: true}))
-		.pipe(gulp.dest('app'))
+		.pipe(gulp.dest('app/js'))
 		.pipe(browserSync.reload({ stream: true }));
 });
 
@@ -49,7 +49,7 @@ gulp.task('sass', function () {
 	return gulp.src('app/sass/**/[^_]*.sass')
 		.pipe(sass({ outputStyle: 'expand' }).on("error", notify.onError()))
 		.pipe(rename({ suffix: '.min', prefix: '' }))
-		.pipe(gulp.dest('app'))
+		.pipe(gulp.dest('app/css'))
 		.pipe(browserSync.reload({ stream: true }));
 });
 
@@ -58,14 +58,17 @@ gulp.task('build', function () {
 	var buildHtml = gulp.src('app/*.html')
 		.pipe(gulp.dest('dist'));
 
-	var buildJs = gulp.src('app/*.js')
-		.pipe(gulp.dest('dist'));
+	var buildJs = gulp.src('app/js/*.js')
+		.pipe(gulp.dest('dist/js'));
 
-	var buildCss = gulp.src('app/*.css')
+	var buildLibs = gulp.src('app/libs/modernizr.js')
+		.pipe(gulp.dest('dist/libs'))
+
+	var buildCss = gulp.src('app/css/*.css')
 		.pipe(autoprefixer(['> 0.01%']))
 		.pipe(gcmq())
 		.pipe(csso({ comments: false }))
-		.pipe(gulp.dest('dist'));
+		.pipe(gulp.dest('dist/css'));
 
 	var buildImg = gulp.src('app/img/**/*')
 		.pipe(imagemin({
