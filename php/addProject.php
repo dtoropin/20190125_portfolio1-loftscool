0eca8e73b2e793e9d7b67a85d59ff9c35861818a
Denis Toropin <denis.toropin@yandex.ru>
2019-02-08 14:35:06 +0300

    add handler for pages

--- php/addProject.php
+++ php/addProject.php
@@ -1,25 +1,25 @@
 <?php
 $file_name = '';
-$upload = './img/sites/';
-// $upload = './upload/';
+//$upload = '/dist/img/sites/';
+$upload = $_SERVER['DOCUMENT_ROOT'] . '/dist/upload/';
 
 // cоздадим папку если её нет
-if(!is_dir($upload)) mkdir($upload, 0777);
+if (!is_dir($upload)) mkdir($upload, 0777);
 
 //сохраняем наш файл на сервер в папку $upload
 if (!empty($_FILES['file']['tmp_name'])) {
 	// $path = $_SERVER['DOCUMENT_ROOT'].'/upload/'.$_FILES['file']['name']; 
-	$path = $upload.$_FILES['file']['name'];
-	(copy($_FILES['file']['tmp_name'], $path)) 
+	$path = $upload . $_FILES['file']['name'];
+	(copy($_FILES['file']['tmp_name'], $path))
 		? $file_name = $_FILES['file']['name']
 		: $file_name = 'no file';
 }
 
 // Обработка данных
-// $result['filename'] = $file_name;
-// $result['name'] = htmlspecialchars($_POST['name']);
-// $result['url'] = htmlspecialchars($_POST['projectUrl']);
-// $result['comment'] = htmlspecialchars($_POST['discr']);
+ $result['filename'] = $file_name;
+ $result['name'] = htmlspecialchars($_POST['name']);
+ $result['url'] = htmlspecialchars($_POST['projectUrl']);
+ $result['comment'] = htmlspecialchars($_POST['discr']);
 
-// die(json_encode($result));
-die();
+ die(json_encode($result));
+//die();
