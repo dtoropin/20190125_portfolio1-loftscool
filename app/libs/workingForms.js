c7da9ab4029c76134ada646625c386777ee5529c
Denis Toropin <denis.toropin@yandex.ru>
2019-02-08 15:36:11 +0300

    update project

---
+++ app/libs/workingForms.js
@@ -0,0 +1,109 @@
+// other
+var workingForms = (function () {
+
+	var _createQtip = function (element, position) {
+		// target для custom input 'file'
+		var target = element.attr('type') === 'file' ? element.siblings('div') : element;
+		// позиция тултипа
+		if (position === 'right') {
+			position = {
+				my: 'center left',
+				at: 'center right',
+				target: target
+			}
+		} else {
+			position = {
+				my: 'center right',
+				at: 'center left',
+				target: target
+			}
+		}
+
+		// инициализация тултипа
+		element.qtip({
+			content: {
+				text: function () {
+					return $(this).attr('qtip-content');
+				}
+			},
+			show: {
+				event: 'show'
+			},
+			hide: {
+				event: 'change keydown hideTooltip'
+			},
+			position: position,
+			style: {
+				classes: 'qtip-mystyle',
+				tip: {
+					border: 0,
+					heigth: 10,
+					width: 16
+				}
+			}
+		}).trigger('show');
+
+	};
+
+	var resetErrorForm = function (form) {
+		$('.qtip').hide();
+		form.find('input, textarea').not('input[type="hidden"]').removeClass('error');
+	};
+
+	var validate = function (form) {
+		var elems = form.find('input, textarea').not('input[type="hidden"]'),
+			valid = true;
+
+		$.each(elems, function (i, val) {
+			var pos = val.hasAttribute('qtip-position')
+				? val.getAttribute('qtip-position')
+				: 'left';
+			if (val.value.length === 0) {
+				val.classList.add('error');
+				_createQtip($(val), pos);
+				valid = false;
+			}
+		});
+		return valid;
+	};
+
+	var ajaxSend = function (url) {
+		var data = new FormData();
+
+		//присоединяем файл
+		data.append('file', $('input[type="file"]')[0].files[0]);
+		//присоединяем остальные поля
+		jQuery.each($('input:not(input[type="file"]), textarea'), function (i, input) {
+			data.append(input.getAttribute('name'), input.value);
+		});
+
+		return $.ajax({
+			url: url,
+			type: 'POST',
+			dataType: 'json',
+			cache: false,
+			contentType: false,
+			processData: false,
+			data: data
+		})
+	};
+
+	var ajaxSendNoFile = function (form, url) {
+		var data = form.serialize();
+
+		return $.ajax({
+			url: url,
+			type: 'POST',
+			dataType: 'json',
+			data: data
+		})
+	};
+
+	return {
+		validate: validate,
+		ajaxSend: ajaxSend,
+		ajaxSendNoFile: ajaxSendNoFile,
+		resetErrorForm: resetErrorForm
+	}
+
+})();
---
+++ gulpfile.js
@@ -0,0 +1,97 @@
+var gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	browserSync = require('browser-sync'),
+	concat = require('gulp-concat'),
+	minify = require('gulp-minify'),
+	csso = require('gulp-csso'),
+	gcmq = require('gulp-group-css-media-queries'),
+	rename = require('gulp-rename'),
+	autoprefixer = require('gulp-autoprefixer'),
+	imagemin = require('gulp-imagemin'),
+	notify = require('gulp-notify');
+
+var js_vendor = [
+	'node_modules/jquery/dist/jquery.js',
+	'app/libs/jquery.bpopup.min.js',
+	'app/libs/qTip/jquery.qtip.min.js',
+	'app/libs/workingForms.js'
+];
+
+// Browsersync
+gulp.task('browser-sync', function () {
+	browserSync({
+		server: {
+			baseDir: 'app'
+		},
+		notify: false,
+		browser: ['chrome']
+	});
+});
+
+// Js-vendor
+gulp.task('js-vendor', function () {
+	return gulp.src(js_vendor)
+		.pipe(concat('vendor.js'))
+		.pipe(minify({noSource: true}))
+		.pipe(gulp.dest('app/js'));
+});
+
+// Js
+gulp.task('js', function () {
+	return gulp.src('app/js-dev/*.js')
+		.pipe(minify({noSource: true}))
+		.pipe(gulp.dest('app/js'))
+		.pipe(browserSync.reload({ stream: true }));
+});
+
+// Sass
+gulp.task('sass', function () {
+	return gulp.src('app/sass/**/[^_]*.sass')
+		.pipe(sass({ outputStyle: 'expand' }).on("error", notify.onError()))
+		.pipe(rename({ suffix: '.min', prefix: '' }))
+		.pipe(gulp.dest('app/css'))
+		.pipe(browserSync.reload({ stream: true }));
+});
+
+// Build
+gulp.task('build', function () {
+	var buildHtml = gulp.src('app/*.html')
+		.pipe(gulp.dest('dist'));
+
+	var buildJs = gulp.src('app/js/*.js')
+		.pipe(gulp.dest('dist/js'));
+
+	var buildLibs = gulp.src('app/libs/modernizr.js')
+		.pipe(gulp.dest('dist/libs'));
+
+	var buildCss = gulp.src('app/css/*.css')
+		.pipe(autoprefixer(['> 0.01%']))
+		.pipe(gcmq())
+		.pipe(csso({ comments: false }))
+		.pipe(gulp.dest('dist/css'));
+
+	var buildImg = gulp.src('app/img/**/*')
+		.pipe(imagemin({
+			interlaced: true, //gif
+			progressive: true, //jpg
+			optimizationLevel: 5, //png
+			svgoPlugins: [{ removeViewBox: false }] //svg
+		}))
+		.pipe(gulp.dest('dist/img'));
+
+	var buildFonts = gulp.src('app/fonts/**/*')
+		.pipe(gulp.dest('dist/fonts'));
+});
+
+// Watch
+gulp.task('watch', ['sass', 'js-vendor', 'js', 'browser-sync'], function () {
+	gulp.watch('app/sass/**/*.sass', function () {
+		setTimeout(function () {
+			gulp.run('sass');
+		}, 1000);
+	});
+	gulp.watch('app/js-dev/**/*.js', ['js']);
+	gulp.watch('app/*.html', browserSync.reload);
+});
+
+gulp.task('default', ['watch']);
